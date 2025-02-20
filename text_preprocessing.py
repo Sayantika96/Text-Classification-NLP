1dcf6acb11a17043e9e132773aca0e5abd051743
sagarr4 <87296736+sagarr4@users.noreply.github.com>
2025-02-20 15:55:44 -0800

    Delete text_preprocessing.py

--- text_preprocessing.py
+++
@@ -1,7 +0,0 @@
-def preprocess_text(text):
-    text = text.lower()
-    text = re.sub(r'\W',' ', text)
-    text = re.sub(r'\s+',' ',text).strip()
-    tokens = word_tokenize(text)
-    tokens = [word for word in tokens if word not in stopwords.words('english')]
-    return " ".join(tokens)
